dd8b23de475dfa2eb993111ae12eaee5fa393fe1
yvz <yvz@DESKTOP-J4Q5LPC>
2022-04-06 15:01:08 +0300

    Add project files.

---
+++ IpScan/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace IpScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(147, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(123, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.ForeColor = System.Drawing.Color.Red;
+            this.textBox3.Location = new System.Drawing.Point(12, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(339, 26);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(276, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Scan";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(362, 71);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 71);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(362, 545);
+            this.panel2.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ShowRowErrors = false;
+            this.dataGridView1.Size = new System.Drawing.Size(362, 545);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 616);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "yavuz.akan@gmail.com";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ IpScan/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace IpScan
+{
+    public partial class Form1 : Form
+    {
+        public static string ipgovde = "";
+        public static string ipkuyruk1 = "";
+        public static string ipkuyruk2 = "";
+        public static string sorip = "";
+        public static string sonuc = "";
+        public static string sonuc2 = "";
+        public static string sonuc3 = "";
+        public static string newline = Environment.NewLine;
+        BackgroundWorker worker;
+       DataTable dt = new DataTable();
+
+      
+        public Form1()
+        {
+            InitializeComponent();
+           
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+
+            dt.Columns.Add("IP"); 
+            dt.Columns.Add("HostName");
+           
+            dataGridView1.DataSource = dt;
+
+            ipgovde = ipbul();
+           dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            textBox1.Text = ipbul()+"1";
+            textBox2.Text = ipbul()+"255";
+            textBox3.Enabled = false;
+        }
+
+
+
+            public string kuyruk11()
+        {
+            string ip1 = textBox1.Text;
+            string ip11 = textBox1.Text;
+            char nokta = '.';
+            char last = ip1[ip1.Length-1];
+            int i = 0;
+            while (last != nokta)
+            {
+                ip1 = ip1.Remove(ip1.Length-1, 1);
+                last = ip1[ip1.Length-1];
+                i++;
+            }
+            string son = ip11.Substring(ip11.Length-i);
+
+            return son;
+
+        }
+
+        public string kuyruk22()
+        {
+            string ip1 = textBox2.Text;
+            string ip11 = textBox2.Text;
+            char nokta = '.';
+            char last = ip1[ip1.Length-1];
+            int i = 0;
+            while (last != nokta)
+            {
+                ip1 = ip1.Remove(ip1.Length-1, 1);
+                last = ip1[ip1.Length-1];
+                i++;
+            }
+            string son = ip11.Substring(ip11.Length-i);
+
+            return son;
+        }
+
+
+
+
+
+        public static string ipbul()
+        {
+            String mac = "";
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    mac = ip.ToString();
+                    char nokta = '.';
+                    char last = mac[mac.Length-1];
+                    while (last != nokta)
+                    {
+
+                        mac = mac.Remove(mac.Length-1, 1);
+
+                        last = mac[mac.Length-1];
+                    }
+
+
+
+                    return mac;
+
+                }
+            }
+            return mac;
+
+
+
+
+        }
+
+       
+
+        public void pingg(string gelenip)
+        {
+            //int timeout = 10;
+            Ping ping = new Ping();
+            PingReply cevap = ping.Send(gelenip);
+
+            if (cevap.Status==IPStatus.Success)
+            {
+              //  textBox3.Text += gelenip + newline;
+            }
+            else
+            {
+
+                // textBox3.Text += gelenip + newline;
+
+            }
+
+
+        }
+
+ 
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            if (backgroundWorker1.IsBusy != true)
+            {
+                dt.Rows.Clear();
+                dataGridView1.Refresh();
+             
+                // Start the asynchronous operation.
+               
+                backgroundWorker1.RunWorkerAsync();
+                button2.Text = "STOP";
+
+            }
+            else 
+            {
+                backgroundWorker1.CancelAsync();
+                button2.Text = "SCAN";
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            String ipkuyruk1 = kuyruk11();
+            String ipkuyruk2 = kuyruk22();
+            int sayi = int.Parse(ipkuyruk1);
+            int sayi2 = int.Parse(ipkuyruk2);
+            int sayi3 = sayi2 + 1;
+
+            if (sayi < 256)
+            {
+
+                for (int i = sayi; i < sayi3; i++)
+                {
+                    if (worker.CancellationPending==true)
+                    { break; }
+
+                  sorip = ipgovde+i.ToString();
+                    // textBox3.Text += ipgovde+i.ToString() + newline;
+                    string time = DateTime.Now.ToString("h:mm:ss tt");
+                    try
+                    {
+
+                        Ping ping = new Ping();
+         
+                        int timeout = 10;
+                      
+
+
+
+                        PingReply cevap = ping.Send(sorip, timeout);
+
+                        if (cevap.Status==IPStatus.Success)
+                        {
+                            sonuc =   sorip;
+                            try
+                            {
+                                IPHostEntry entry = Dns.GetHostEntry(sorip);
+                                if (entry != null)
+                                {
+                                     sonuc3 = entry.HostName;
+                                 
+                                }
+
+                            }
+                            catch
+                            {
+                            }
+                        }
+                        else
+                        {
+
+                            sonuc = "";
+
+                        }
+                        sonuc2 = sorip;
+
+                    }
+                    catch (Exception exp)
+                    {
+
+
+                    }
+
+                    System.Threading.Thread.Sleep(1);
+                    worker.ReportProgress(1);
+
+                }
+
+            }
+            else
+            {
+
+            }
+      
+
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            textBox3.Text = sonuc2;
+            DataRow dr = dt.NewRow();
+            if(sonuc != "")
+            {            
+                dr[0]= sonuc;
+                dr[1]= sonuc3;
+                dt.Rows.Add(dr);
+                sonuc = "";
+                sonuc3 = "";
+
+            }
+        }
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //textBox3.Text += "end";
+        }
+    }
+}
